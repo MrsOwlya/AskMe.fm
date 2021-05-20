79f30cb3943a4632dfbd55ec59dc839e66a0a82d
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 01:26:42 +0300

    Added some pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Указываем, куда откуда переходим(по какому адресу)
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('asking/', include('asking.urls')),
