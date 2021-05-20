eb0b574b67bfe025d462f1e3c1180b5665399d73
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 01:22:38 +0300

    Added some pages

--- mysite/apps/asking/urls.py
+++ mysite/apps/asking/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'asks'
+app_name = 'asking'
 urlpatterns = [
-	path('', views.index, name = 'index'),
-	path('<int:ask_id>/', views.detail, name = 'detail'),
-	path('<int:ask_id>/get_answer/', views.get_answer, name = 'get_answer'),
+	path('', views.index, name = 'asking/index'),
+	path('login/', views.login, name = 'asking/login'),
+	path('signup/', views.signup, name = 'asking/signup'),
+	path('<int:ask_id>/', views.question, name = 'asking/question'),
+	path('<int:ask_id>/get_answer/', views.get_answer, name = 'asking`/get_answer'),
 ]
