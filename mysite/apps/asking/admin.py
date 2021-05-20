93ce13ad8834a96d130ef425cb947bd1c923a662
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 01:19:31 +0300

    Added User, Tag, Like

--- mysite/apps/asking/admin.py
+++ mysite/apps/asking/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Ask, Answer
+from .models import User, Ask, Answer, Tag, Like
 
 # Register your models here.
 
 admin.site.register(Ask)
 admin.site.register(Answer)
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Like)
