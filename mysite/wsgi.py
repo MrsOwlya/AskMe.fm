e7f6ac487fa18b33526ad30199da4c2bfbc9d047
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 01:28:27 +0300

    Why does it changed???

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
+# Это файл для выгрузки кода сайта
 import os
 
 from django.core.wsgi import get_wsgi_application
