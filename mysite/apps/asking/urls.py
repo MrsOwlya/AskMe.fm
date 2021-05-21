244b28a9a130f0f1f2d642f6b2bd42447778a336
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 15:58:06 +0300

    New urls

--- mysite/apps/asking/urls.py
+++ mysite/apps/asking/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'asking'
 urlpatterns = [
-	path('', views.index, name = 'asking/index'),
-	path('login/', views.login, name = 'asking/login'),
-	path('signup/', views.signup, name = 'asking/signup'),
-	path('<int:ask_id>/', views.question, name = 'asking/question'),
-	path('<int:ask_id>/get_answer/', views.get_answer, name = 'asking`/get_answer'),
+	path('', views.index, name = 'index'),
+	path('login/', views.login, name = 'login'),
+	path('signup/', views.signup, name = 'signup'),
+	path('ask/', views.ask, name='ask'),
+	path('<int:ask_id>/', views.question, name = 'question'),
 ]
