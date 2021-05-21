81fb1dfbbb954a223d57410ad606ad7e56445f95
MrsOwlya <olja-holmes@mail.ru>
2021-05-21 15:59:47 +0300

    Changed index

--- mysite/apps/asking/views.py
+++ mysite/apps/asking/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
-from .models import Ask, Answer
+from .models import Ask, Answer, User, Like, Tag
 
 def base(request):
 	return render(request, 'base.html')
@@ -12,16 +12,14 @@ def login(request):
 def signup(request):
 	return render(request, 'asking/signup.html')
 
+def ask(request):
+	return render(request, 'asking/ask.html')
+
 def index(request):
-	latest_asks_list = Ask.objects.order_by('-ask_date')[:5]
-	return render(request, 'asking/index.html', {'latest_asks_list': latest_asks_list})
+	index = Ask.objects.order_by('-ask_date')[:5]
+	return render(request, 'asking/index.html', {'index': index})
 	
-def question(request, ask_id):
-	try:
-		a = Ask.objects.get( id = ask_id )
-	except:
-		raise ObjectDoesNotExist("Вопрос не найден!")
-	last_answers_list = a.answer_set.order_by('-id')[:10]
-		
-	return render(request, 'asking/question.html', {'ask': a})
+def question(request):
+
+	return render(request, 'asking/question.html')
 # Create your views here.
