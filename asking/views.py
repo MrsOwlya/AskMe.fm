0794815b15eb89d93ae38c79b87e2983b55e6138
MrsOwlya <olja-holmes@mail.ru>
2021-05-24 20:04:32 +0300

    Added new class for questions

--- asking/views.py
+++ asking/views.py
@@ -5,14 +5,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from .models import Ask, Answer, Account
 from .forms import SignupForm, LoginForm, AskForm, AnswerForm
-
-
-def login(request):
-	return render(request, 'asking/login.html')
-
-
-def signup(request):
-	return render(request, 'asking/signup.html')
+from django.views.generic import DetailView
 
 
 def signup_up(request):
@@ -24,11 +17,10 @@ def signup_up(request):
 				user = User.objects.create_user(username=request.POST.get('user_login'), email=request.POST.get('user_email'), password=request.POST.get('user_password'))
 				user.save()
 				user_id = User.objects.get(id=user.pk)
-				avatar = Account(user=user_id, avatar=request.POST.get('user_avatar'))
+				avatar = Account(user=user_id, user_avatar=request.POST.get('user_avatar'))
 				avatar.save()
 			except IntegrityError:
 				return render(request, 'asking/signup.html', {'form': form})
-			return HttpResponseRedirect('/?continue=login')
 		return render(request, 'asking/signup.html', {'form': form})
 	return render(request, 'asking/signup.html', {'form': form})
 
@@ -40,17 +32,17 @@ def ask(request):
 			question = Ask.objects.create(ask_title=request.POST.get('ask_title'), ask_explane=request.POST.get('ask_explane'), asker_name=request.user, ask_tags=request.POST.get('ask_tags'))
 			question.save()
 			return render(request, 'asking/index.html')
-		return render(request, 'asking/ask.html', {'form': form})
-	return render(request, 'asking/ask.html', {'form': form})
+		return render(request, 'asking/ask.html', {'form': form, 'avatar': avatar(request)})
+	return render(request, 'asking/ask.html', {'form': form, 'avatar': avatar(request)})
 
 def index(request):
 	index = Ask.objects.order_by('-ask_date')[:5]
-	return render(request, 'asking/index.html', {'index': index})
+	return render(request, 'asking/index.html', {'index': index, 'avatar': avatar(request)})
 
-
-def question(request):
-	answer = Answer.objects.order_by('-answer_date')
-	return render(request, 'asking/question.html')
+class QuestDetailView(DetailView):
+	model = Ask
+	template_name = 'asking/question.html'
+	context_object_name = 'quest'
 
 
 def login_in(request):
@@ -70,4 +62,14 @@ def logout(request):
 	if request.user.is_authenticated:
 		auth.logout(request)
 	return HttpResponseRedirect('/?continue=logout')
+
+def avatar(request):
+	avatar = None
+	try:
+		if request.user.is_authenticated:
+			avatar=Account.objects.get(user_id=request.user.id).user_avatar.url
+	except ValueError:
+		pass
+	return avatar
+
 # Create your views here.
