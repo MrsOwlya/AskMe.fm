d8d9d0f632319103d4bc97ab683135ea56444179
MrsOwlya <olja-holmes@mail.ru>
2021-05-24 20:03:23 +0300

    Added pages for every question

--- asking/urls.py
+++ asking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from mysite.settings import MEDIA_URL, MEDIA_ROOT
-from .views import index, ask, question, login_in, signup_up, logout
+from .views import index, ask, login_in, signup_up, logout, QuestDetailView
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import handler404
@@ -10,7 +10,7 @@ urlpatterns = [
 	path('login/', login_in, name='login'),
 	path('signup/', signup_up, name='signup'),
 	path('ask/', ask, name='ask'),
-	path('<int:ask_id>/', question, name='question'),
+	path('<int:pk>/', QuestDetailView.as_view(), name='question'),
 	path('logout/', logout, name='logout')
 ]
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
